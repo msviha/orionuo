84ef790e93d4f9f06bfabccaacf899c998dbe087
MSviha <msviha@ra.rockwell.com>
2020-09-18 08:17:18 +0200

    some attempts with list

--- src/scripts/klamak.ts
+++ src/scripts/klamak.ts
@@ -11,19 +11,30 @@ namespace Scripts {
          */
         static next() {
             const lists = ['petlvl1', 'petlvl2', 'petlvl3', 'petlvl4', 'petlvl5'];
-            const current = Orion.GetGlobal('klamak');
+            let current = Orion.GetGlobal('klamak');
 
             if (!current) {
-                Orion.SetGlobal('klamak', lists[0]);
+                Orion.SetGlobal('klamak', lists[lists.length - 1]);
+                current = Orion.GetGlobal('klamak');
             }
-            else {
-                const currentIndex = lists.indexOf(current);
-                const nextIndex = currentIndex + 1 === lists.length ? 0 : currentIndex + 1;
-                Orion.SetGlobal('klamak', lists[nextIndex])
+
+            let currentIndex = lists.indexOf(current);
+            let nextIndex:number;
+            let i = 0;
+            do {
+                i++;
+                nextIndex = (currentIndex + i) % lists.length;
+                Orion.SetGlobal('klamak', lists[nextIndex]);
+            } while (i < lists.length && !Orion.FindList(Orion.GetGlobal('klamak')).length)
+
+            const numberOfKlamaks = Orion.FindList(Orion.GetGlobal('klamak')).length;
+            if (!numberOfKlamaks) {
+                Scripts.Utils.playerPrint('nemas klamaky', ColorEnum.red);
+                return;
             }
 
             const nextList = Orion.GetGlobal('klamak');
-            Scripts.Utils.playerPrint(nextList);
+            Scripts.Utils.playerPrint(`${nextList} [${numberOfKlamaks}]`);
         }
     }
 }
