2cb5cf4805e6af093c376ddc775ca3321baa828d
MSviha <msviha@ra.rockwell.com>
2020-12-27 23:34:57 +0100

    0.1.5 mount from backpack

--- src/scripts/mount.ts
+++ src/scripts/mount.ts
@@ -12,12 +12,61 @@ namespace Scripts {
                 Orion.UseObject('self');
             }
             else if (!Orion.FindObject('myMount')) {
-                this.addMount();
+                Scripts.Mount.resolveNewMount();
             }
             else {
                 Orion.UseObject('myMount');
             }
-            Orion.Wait(50)
+        }
+
+        static resolveNewMount() {
+            Orion.ClearJournal();
+            Orion.Wait(50);
+
+            if (Scripts.Mount.mountMyPet()) {
+                return;
+            }
+
+            Scripts.Mount.unshrinkAndMount();
+        }
+
+        static mountMyPet():boolean {
+            const mountsGraphic = '0x00DF|0x00DC|0x00DA|0x00E2|0x00CC|0x00E4|0x00D2|0x00DB|0x00C8';
+            const groundPets = Orion.FindType(mountsGraphic, '0xFFFF', 'ground', 'live', 5);
+            for (const pet of groundPets) {
+                const petObject = Orion.FindObject(pet);
+                if (petObject.CanChangeName()) {
+                    Orion.AddObject('myMount', pet);
+                    Orion.UseObject(pet);
+                    if (!Orion.InJournal('reach the creature')) {
+                        return true;
+                    }
+                    Orion.ClearJournal();
+                }
+            }
+            return false;
+        }
+
+        static unshrinkAndMount() {
+            const shrinkedMountsGraphic = '0x211F|0x2121|0x2124|0x20F6|0x2120|0x2135|0x2136|0x2137|0x20DD';
+            const shrinkedMounts = Orion.FindType(shrinkedMountsGraphic, '0xFFFF');
+            const mountsGraphic = '0x00DF|0x00DC|0x00DA|0x00E2|0x00CC|0x00E4|0x00D2|0x00DB|0x00C8';
+            for (const m of shrinkedMounts) {
+                Orion.ClearJournal();
+                Orion.UseObject(m);
+                Orion.Wait(responseDelay);
+                const groundPets = Orion.FindType(mountsGraphic, '0xFFFF', 'ground', 'live', 5);
+                if (!Orion.InJournal('You cannot unshrink')) {
+                    for (const pet of groundPets) {
+                        const petObject = Orion.FindObject(pet);
+                        if (petObject.CanChangeName()) {
+                            Orion.AddObject('myMount', pet);
+                            Orion.UseObject(pet);
+                            break;
+                        }
+                    }
+                }
+            }
         }
 
         /**
